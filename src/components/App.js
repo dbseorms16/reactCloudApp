beb5fde30637657b89a5102a3a66c8c8e52206d1
Jun seok <dbseorms16@naver.com>
2020-09-09 01:20:06 +0900

    Add Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import AppShell from "./AppShell";
+import Home from "./Home";
+import Text from "./Text";
+import Words from "./Words";
 class App extends Component {
   render() {
-    return <AppShell />;
+    return (
+      <Router>
+        <AppShell>
+          <div>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/texts" component={Text}></Route>
+            <Route exact path="/words" component={Words}></Route>
+          </div>
+        </AppShell>
+      </Router>
+    );
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+class Home extends Component {
+  render() {
+    return (
+      <Card>
+        <CardContent>리액트 파이어베이스 기반</CardContent>
+      </Card>
+    );
+  }
+}
+
+export default Home;
